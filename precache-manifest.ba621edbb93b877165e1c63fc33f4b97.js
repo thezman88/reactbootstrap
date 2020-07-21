4939d028502e8289ac36d9bf72eb2caea943cbc0
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2020-07-21 15:53:36 -0400

    Updates

---
+++ precache-manifest.ba621edbb93b877165e1c63fc33f4b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fee3f2beb6f3fc80a8e47758fbda5f",
+    "url": "/reactbootstrap/index.html"
+  },
+  {
+    "revision": "22bf2a72c1d7ace3c334",
+    "url": "/reactbootstrap/static/css/main.be0ef2d0.chunk.css"
+  },
+  {
+    "revision": "26130c92bf2dbca8e96a",
+    "url": "/reactbootstrap/static/js/2.5e6e90ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactbootstrap/static/js/2.5e6e90ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22bf2a72c1d7ace3c334",
+    "url": "/reactbootstrap/static/js/main.ca64da86.chunk.js"
+  },
+  {
+    "revision": "0790ea3d5468c6c51179",
+    "url": "/reactbootstrap/static/js/runtime-main.f9b941d2.js"
+  }
+]);
